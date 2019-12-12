183ab632544545a2947e567ea3f210b580a54d71
aswanabdulah <56129388+aswanabdulah@users.noreply.github.com>
2019-12-13 08:42:45 +0900

    Praktikum 7

---
+++ Pgame-16091/code71.py
@@ -0,0 +1,17 @@
+# Pemrograman Game Praktikum 7
+# latihan code 7.1 : Pgame
+
+import pygame, sys
+from pygame.locals import*
+
+pygame.init()
+
+DYSPLAYSURF = pygame.display.set_mode ((400, 300))
+
+pygame.display.set_caption ('Hello My World!')
+
+while True: # main game loop
+	for event in pygame.event.get ();
+	if event.type == QUIT:
+		pygame.quit ()
+	pygame.display.update ()
